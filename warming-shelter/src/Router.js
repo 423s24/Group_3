cde1122064ad92c315631c90d4e9d85469aafc72
Shane Costello <143199537+ShaneCost@users.noreply.github.com>
2024-02-21 10:35:57 -0700

    Update Router.js

--- warming-shelter/src/Router.js
+++ warming-shelter/src/Router.js
@@ -2,11 +2,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from "./views/Dashboard.vue";
 import Login from "./views/Login.vue";
+import Checkin from "./views/Checkin.vue";
 
 
 const routes = [
     { path: '/', component: Dashboard },
     { path: '/login', component: Login },
+    { path: '/checkin', component: Checkin}
 ]
 
 const router = createRouter({
