1129f48b9888fbd81b7c32bbdb1ccb34421848da
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2024-02-21 10:33:42 -0700

    Implement login page

--- warming-shelter/src/Router.js
+++ warming-shelter/src/Router.js
@@ -1,10 +1,12 @@
 // defining the routes for the application
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from "./views/Dashboard.vue";
+import Login from "./views/Login.vue";
 
 
 const routes = [
     { path: '/', component: Dashboard },
+    { path: '/login', component: Login },
 ]
 
 const router = createRouter({
---
+++ warming-shelter/src/views/Login.vue
@@ -0,0 +1,24 @@
+<!-- Login view -->
+<template>
+    <form>
+        <div>
+            <h1>Login</h1>
+            <sub>Please sign-in to continue</sub>
+        </div>
+        <div>
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" required>
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" required>
+        </div>
+        <div>
+            <button type="submit">Login</button>
+            <button type="reset">Cancel</button>
+        </div>
+    </form>
+</template>
+
+<script setup lang="ts">
+</script>
