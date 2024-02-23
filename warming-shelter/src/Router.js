bf61691680d397f22cb24f0371e038419b559f45
Jonathan Neuman <jonrileyneuman@gmail.com>
2024-02-23 13:27:27 -0700

    Added header routing for existing pages

--- warming-shelter/src/Router.js
+++ warming-shelter/src/Router.js
@@ -1,6 +1,7 @@
 // defining the routes for the application
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from "./views/Dashboard.vue";
+import EnterGuests from "./views/EnterGuests.vue"
 import Login from "./views/Login.vue";
 import Checkin from "./views/Checkin.vue";
 
@@ -8,6 +9,7 @@ import Checkin from "./views/Checkin.vue";
 const routes = [
     { path: '/', component: Login },
     { path: '/dashboard', component: Dashboard },
+    { path: '/enterguests', component: EnterGuests},
     { path: '/checkin', component: Checkin}
 ]
 
---
+++ warming-shelter/src/views/EnterGuests.vue
@@ -0,0 +1,13 @@
+<template>
+    <Header />
+</template>
+
+<script>
+    import Header from '../components/Header.vue'
+    export default {
+        name: 'EnterGuests',
+        components:{
+            Header
+        }
+    }
+</script>
