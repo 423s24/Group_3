e14c03ce566f95bb771db6af5453f6d431403de1
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2024-02-23 11:58:50 -0700

    Tweaked the styling and implement the state store to dispatch the login function from the AuthModule.js

--- warming-shelter/src/views/Login.vue
+++ warming-shelter/src/views/Login.vue
@@ -1,21 +1,28 @@
 <template>
   <section class="h-screen bg-hrdc-teal">
     <div class="flex flex-col items-center justify-center px-6 mx-auto md:h-screen lg:py-0">
-      <a href="/dashboard" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
-        <img class="w-20 h-15" src="../assets/hrdc-logo.png" alt="logo">
-      </a>
+      <div class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+        <img class="" src="../assets/hrdc-220.png" alt="logo">
+      </div>
       <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
         <div>
-          <form @submit="handleFormSubmit">
+          <form @submit.prevent="submitForm">
             <div>
               <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Username</label>
-              <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required>
+              <input v-model="username" type="text" name="username" id="username"
+                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                     required>
             </div>
             <div>
               <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
-              <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required>
+              <input v-model="password" type="password" name="password" id="password"
+                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                     required>
             </div>
-            <button type="submit" class="w-full text-white bg-hrdc-orange hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Login</button>
+            <button type="submit"
+                    class="login-btn w-full text-white bg-hrdc-orange focus:ring-4 focus:outline-none focus:ring-opacity-5 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+              Login
+            </button>
           </form>
         </div>
       </div>
@@ -23,9 +30,29 @@
   </section>
 </template>
 
-<script setup lang="ts">
-function handleFormSubmit(event: Event) {
-  event.preventDefault()
-  alert('Form submitted')
+<style scoped>
+.login-btn:hover {
+  opacity: 0.8;
 }
+</style>
+
+<script>
+import store from "../store/store.js";
+
+export default {
+  data: () => {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      store.dispatch("authModule/login", {
+        username: this.username,
+        password: this.password,
+      });
+    },
+  }
+};
 </script>
