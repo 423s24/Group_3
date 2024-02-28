a3e15e0c4c4ac62b1415e0ea8d755df3036f510c
sagejames1 <sageajames1@gmail.com>
2024-02-28 13:45:53 -0700

    added CounterCard component

--- warming-shelter/src/views/Dashboard.vue
+++ warming-shelter/src/views/Dashboard.vue
@@ -1,35 +1,37 @@
-<!-- homepage view -->
 <template>
-    <Header />
-    <br>
-    <Wrapper>
-        <h2>
-            Motivation
-        </h2>
-
-        <p>
-            The primary motivation behind the development of this software stems from the need to streamline and enhance the efficiency of managing guest data at the HRDC's homeless warming center. Prior to the introduction of this software, the center relied heavily on manual data entry processes, utilizing Google Sheets to input and manage information across two separate databases: one internal (CaseWorthy) and one statewide (HMIS). This method was not only time-consuming but also prone to errors, leading to inefficiencies in data management and reporting.
-        </p>
+    <div>
+      <Header />
+      <br>
+      <Wrapper>
+        <div class="flex">
+            <CounterCard title="Card Title" content="Card content goes here" />
+            <CounterCard title="Card Title" content="Card content goes here" />
+            <CounterCard title="Card Title" content="Card content goes here" />
+        </div>
+  
+        <h2>Motivation</h2>
+        <p>The primary motivation behind the development of this software stems from the need to streamline and enhance the efficiency of managing guest data at the HRDC's homeless warming center. Prior to the introduction of this software, the center relied heavily on manual data entry processes, utilizing Google Sheets to input and manage information across two separate databases: one internal (CaseWorthy) and one statewide (HMIS). This method was not only time-consuming but also prone to errors, leading to inefficiencies in data management and reporting.</p>
+        
         <br>
-        <h2>
-            Purpose
-        </h2>
-
-        <p>
-            This custom software solution is designed to enhance the operation of the HRDC’s year-round homeless warming center in Bozeman, Montana. It aims to assist front desk staff in managing incoming and outgoing guests efficiently. This software aims to minimize the learning curve and reduce the operation burden on front desk staff.
-        </p>
-    </Wrapper>
-</template>
-
-<script>
-    import Header from '../components/Header.vue'
-    import Wrapper from '../components/Wrapper.vue'
-    export default {
-        name: 'Dashboard',
-        components:{
-            Header, 
-            Wrapper
-        }
+        
+        <h2>Purpose</h2>
+        <p>This custom software solution is designed to enhance the operation of the HRDC’s year-round homeless warming center in Bozeman, Montana. It aims to assist front desk staff in managing incoming and outgoing guests efficiently. This software aims to minimize the learning curve and reduce the operation burden on front desk staff.</p>
+      </Wrapper>
+    </div>
+  </template>
+  
+  <script>
+  import Header from '../components/Header.vue'
+  import Wrapper from '../components/Wrapper.vue'
+  import CounterCard from '../components/CounterCard.vue'
+  
+  export default {
+    name: 'Dashboard',
+    components: {
+      Header,
+      Wrapper,
+      CounterCard
     }
-</script>
-
+  }
+  </script>
+  
