feeaf85f618e190280d0cbc69962465bde55b944
ShaneCost <shanecost2002@gmail.com>
2024-03-04 17:51:50 -0700

    updated routes

--- warming-shelter-restapi/src/routes/guestRoutes.ts
+++ warming-shelter-restapi/src/routes/guestRoutes.ts
@@ -7,9 +7,13 @@ dotenv.config();
 
 const guestRouter = Router();
 
-// Get All Users Route
+// Get All Guests Route
 guestRouter.get('/all', GuestController.getAllGuests);
+
+// Get One Guest by ID Route
 guestRouter.get('/viewProfile/:id', GuestController.viewProfile);
+
+// Get All Current Guests Route
 guestRouter.get('/current', GuestController.getCurrentGuests);
 
 export default guestRouter;
