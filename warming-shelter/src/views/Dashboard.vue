5f450a00ffb77e01e994a6beb2048e986aeac6bf
sagejames1 <sageajames1@gmail.com>
2024-03-08 07:36:12 -0700

    adding note section to dashboard

--- warming-shelter/src/views/Dashboard.vue
+++ warming-shelter/src/views/Dashboard.vue
@@ -29,73 +29,13 @@ export default {
   },
 
 =======
-    <div>
-      <Header />
-      <br>
-      <Wrapper>
-        <div class="flex">
-            <CounterCard title="23 Current Guests" content="Card content goes here" />
-            <CounterCard title="26 Bunks Available" content="Card content goes here" />
-            <CounterCard title="18 Lockers Available" content="Card content goes here" />
-        </div>
 
-        <NoteSection NoteTitle="Guest Notes"/>
-        <NoteSection NoteTitle="Nightly Celebrations"/>
-
-        
-      </Wrapper>
-    </div>
-  </template>
-  
-  <script>
-  import Header from '../components/Header.vue'
-  import Wrapper from '../components/Wrapper.vue'
-  import CounterCard from '../components/CounterCard.vue'
-  import NoteSection from '../components/NoteSection.vue'
-  
-  export default {
-    name: 'Dashboard',
-    components: {
-      Header,
-      Wrapper,
-      CounterCard,
-      NoteSection
-    },
-  
 >>>>>>> Stashed changes
   data() {
     return {
       firstName: '',
       lastName: '',
-      sites: [
-        {
-          uid: 1,
-          name: 'WebDigitalCommunications',
-          link: 'https://www.montana.edu/web/',
-          errors: 0,
-          page: 1,
-          docs: 0,
-          lastScanned: 'January 25, 2024'
-        },
-        {
-          uid: 2,
-          name: 'ComputerScience',
-          link: 'https://www.montana.edu/cs/',
-          errors: 0,
-          page: 1,
-          docs: 0,
-          lastScanned: 'January 1, 2022'
-        },
-        {
-          uid: 3,
-          name: 'ComputerScience',
-          link: 'https://www.montana.edu/cs/',
-          errors: 0,
-          page: 1,
-          docs: 0,
-          lastScanned: 'January 1, 2022'
-        }
-      ]
+
     }
   },
   async created() {
