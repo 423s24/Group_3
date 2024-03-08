87a5b03d80de05e4d59c3534716d595761c81adf
sagejames1 <sageajames1@gmail.com>
2024-03-08 07:44:50 -0700

    revised dashboard

--- warming-shelter/src/views/Dashboard.vue
+++ warming-shelter/src/views/Dashboard.vue
@@ -1,41 +1,73 @@
 <template>
-<<<<<<< Updated upstream
   <div>
     <Header/>
     <br>
     <Wrapper>
-      <h1>Welcome, {{ firstName }} {{lastName}}!</h1>
+      <h1 class="text-center">Welcome, {{ firstName }} {{lastName}}!</h1>
       <div class="flex">
         <CounterCard title="Card Title" content="Card content goes here"/>
         <CounterCard title="Card Title" content="Card content goes here"/>
         <CounterCard title="Card Title" content="Card content goes here"/>
       </div>
+
+        <NoteSection NoteTitle="Guest Notes"/>
+        <NoteSection NoteTitle="Nightly Celebrations"/>
+
     </Wrapper>
   </div>
 </template>
 
-<script>
-import Header from '../components/Header.vue'
-import Wrapper from '../components/Wrapper.vue'
-import CounterCard from '../components/CounterCard.vue'
-import store from "../store/store.js";
 
-export default {
-  name: 'Dashboard',
-  components: {
-    Header,
-    Wrapper,
-    CounterCard
-  },
-
-=======
-
->>>>>>> Stashed changes
+  
+  <script>
+  import Header from '../components/Header.vue'
+  import Wrapper from '../components/Wrapper.vue'
+  import CounterCard from '../components/CounterCard.vue'
+  import NoteSection from '../components/NoteSection.vue'
+  import store from "../store/store.js";
+  
+  export default {
+    name: 'Dashboard',
+    components: {
+      Header,
+      Wrapper,
+      CounterCard,
+      NoteSection
+    },
+  
   data() {
     return {
       firstName: '',
       lastName: '',
-
+      sites: [
+        {
+          uid: 1,
+          name: 'WebDigitalCommunications',
+          link: 'https://www.montana.edu/web/',
+          errors: 0,
+          page: 1,
+          docs: 0,
+          lastScanned: 'January 25, 2024'
+        },
+        {
+          uid: 2,
+          name: 'ComputerScience',
+          link: 'https://www.montana.edu/cs/',
+          errors: 0,
+          page: 1,
+          docs: 0,
+          lastScanned: 'January 1, 2022'
+        },
+        {
+          uid: 3,
+          name: 'ComputerScience',
+          link: 'https://www.montana.edu/cs/',
+          errors: 0,
+          page: 1,
+          docs: 0,
+          lastScanned: 'January 1, 2022'
+        }
+      ]
     }
   },
   async created() {
