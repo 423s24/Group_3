38469de4e4c67c495bee3c59c46dea7a57e7d0d1
ShaneCost <shanecost2002@gmail.com>
2024-04-22 18:09:18 -0600

    aaa

--- warming-shelter/src/views/AssignSpaces.vue
+++ warming-shelter/src/views/AssignSpaces.vue
@@ -3,103 +3,124 @@
         <Header />
         <br>
         <Wrapper>
-          <button class="checkout-btn" @click="checkoutAll">Clear All</button>
-          <div class="table-container">
-            <div class="float-left">
-              <p class="text-2xl text-bg-blue-900"> {{ reservations.length }} Reservations</p>
-              <table class="w-full border-2 border-bg-blue-900">
+          <div v-if="viewBunks">
+            <button @click="switchView" class="switch-btn">Edit lockers</button>
+            <button class="checkout-btn" @click="checkoutAll">Clear All</button>
+            <div class="table-container">
+              <div class="float-left">
+                <p class="text-2xl text-bg-blue-900"> {{ reservations.length }} Reservations</p>
+                <table class="w-full border-2 border-bg-blue-900">
+                    <thead class="bg-blue-900">
+                        <tr>
+                            <th class="text-left p-3 text-white">Bunk<i class="bi-alarm"></i></th>
+                            <th class="text-left p-3 text-white">Guest</th>
+                            <th class="text-left p-3 text-white"></th>
+                        </tr>
+                    </thead>
+                    <tbody class="rounded">
+                        <tr 
+                          class="border-b-2 border-bg-blue-900 h-14" 
+                          v-for="(guest, index) in reservations" 
+                          :key="index"
+                          :class="{ 'bg-white': index % 2 === 0, 'bg-yellow-100': index % 2 !== 0 }"
+                        >
+                            <td class="px-3">{{ guest.bunk_reservation_number }}</td>
+                            <td class="px-3">{{ guest.firstName }} {{ guest.lastName }}</td>
+                            <td class="px-3">
+                              <router-link :to="{ name: 'GuestProfile', params: { id: guest.id } }">
+                                <button class="bg-blue-900 hover:bg-gray-500 text-white rounded px-3 py-2">View Profile</button>
+                              </router-link>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+              </div>
+        
+
+              <!-- Observation table -->
+              <div class="float-left">
+                <p class="text-2xl text-bg-blue-900">Observation</p>
+                <table class="w-full border-2 border-bg-blue-900">
                   <thead class="bg-blue-900">
-                      <tr>
-                          <th class="text-left p-3 text-white">Bunk<i class="bi-alarm"></i></th>
-                          <th class="text-left p-3 text-white">Guest</th>
-                          <th class="text-left p-3 text-white"></th>
-                      </tr>
+                    <tr>
+                      <th class="text-left p-3 text-white">Bunk<i class="bi-alarm"></i></th>
+                      <th class="text-left p-3 text-white">Guest</th>
+                    </tr>
                   </thead>
                   <tbody class="rounded">
-                      <tr 
-                        class="border-b-2 border-bg-blue-900 h-14" 
-                        v-for="(guest, index) in reservations" 
-                        :key="index"
-                        :class="{ 'bg-white': index % 2 === 0, 'bg-yellow-100': index % 2 !== 0 }"
-                      >
-                          <td class="px-3">{{ guest.bunk_reservation_number }}</td>
-                          <td class="px-3">{{ guest.firstName }} {{ guest.lastName }}</td>
-                          <td class="px-3">
-                            <router-link :to="{ name: 'GuestProfile', params: { id: guest.id } }">
-                              <button class="bg-blue-900 hover:bg-gray-500 text-white rounded px-3 py-2">View Profile</button>
-                            </router-link>
-                          </td>
-                      </tr>
+                    <!-- Loop for observation slots -->
+                    <tr v-for="bunk in filteredBunks('observation')" :key="bunk.number">
+                      <td class="px-3">{{ bunk.number }}</td>
+                      <td class="px-3">
+                        <input type="text" v-model="bunk.name" @input="updateBunk(bunk.number, bunk.name)" class="border-2 border-gray-500 rounded px-3 py-1 w-full">
+                      </td>
+                    </tr>
                   </tbody>
-              </table>
-            </div>
+                </table>
+              </div>
 
-            <!-- Observation table -->
-            <div class="float-left">
-              <p class="text-2xl text-bg-blue-900">Observation</p>
-              <table class="w-full border-2 border-bg-blue-900">
-                <thead class="bg-blue-900">
-                  <tr>
-                    <th class="text-left p-3 text-white">Bunk<i class="bi-alarm"></i></th>
-                    <th class="text-left p-3 text-white">Guest</th>
-                  </tr>
-                </thead>
-                <tbody class="rounded">
-                  <!-- Loop for observation slots -->
-                  <tr v-for="bunk in filteredBunks('observation')" :key="bunk.number">
-                    <td class="px-3">{{ bunk.number }}</td>
-                    <td class="px-3">
-                      <input type="text" v-model="bunk.name" @input="updateBunk(bunk.number, bunk.name)" class="border-2 border-gray-500 rounded px-3 py-1 w-full">
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+              <!-- Women's table -->
+              <div class="float-left">
+                <p class="text-2xl text-bg-blue-900">Women's</p>
+                <table class="w-full border-2 border-bg-blue-900">
+                  <thead class="bg-blue-900">
+                    <tr>
+                      <th class="text-left p-3 text-white">Bunk<i class="bi-alarm"></i></th>
+                      <th class="text-left p-3 text-white">Guest</th>
+                    </tr>
+                  </thead>
+                  <tbody class="rounded">
+                    <!-- Loop for women's slots -->
+                    <tr v-for="bunk in filteredBunks('women')" :key="bunk.number">
+                      <td class="px-3">{{ bunk.number }}</td>
+                      <td class="px-3">
+                        <input type="text" v-model="bunk.name" @input="updateBunk(bunk.number, bunk.name)" class="border-2 border-gray-500 rounded px-3 py-1 w-full">
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
 
-            <!-- Women's table -->
-            <div class="float-left">
-              <p class="text-2xl text-bg-blue-900">Women's</p>
-              <table class="w-full border-2 border-bg-blue-900">
-                <thead class="bg-blue-900">
-                  <tr>
-                    <th class="text-left p-3 text-white">Bunk<i class="bi-alarm"></i></th>
-                    <th class="text-left p-3 text-white">Guest</th>
-                  </tr>
-                </thead>
-                <tbody class="rounded">
-                  <!-- Loop for women's slots -->
-                  <tr v-for="bunk in filteredBunks('women')" :key="bunk.number">
-                    <td class="px-3">{{ bunk.number }}</td>
-                    <td class="px-3">
-                      <input type="text" v-model="bunk.name" @input="updateBunk(bunk.number, bunk.name)" class="border-2 border-gray-500 rounded px-3 py-1 w-full">
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+              <!-- Men's table -->
+              <div class="float-left">
+                <p class="text-2xl text-bg-blue-900">Men's</p>
+                <table class="w-full border-2 border-bg-blue-900">
+                  <thead class="bg-blue-900">
+                    <tr>
+                      <th class="text-left p-3 text-white">Bunk<i class="bi-alarm"></i></th>
+                      <th class="text-left p-3 text-white">Guest</th>
+                    </tr>
+                  </thead>
+                  <tbody class="rounded">
+                    <!-- Loop for men slots -->
+                    <tr v-for="bunk in filteredBunks('men')" :key="bunk.number">
+                      <td class="px-3">{{ bunk.number }}</td>
+                      <td class="px-3">
+                        <input type="text" v-model="bunk.name" @input="updateBunk(bunk.number, bunk.name)" class="border-2 border-gray-500 rounded px-3 py-1 w-full">
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="clearfix"></div> <!-- Clear the float -->
             </div>
+          </div>
+          
+          <div v-else>
+            <button @click="switchView" class="switch-btn">Edit bunks</button>
 
-            <!-- Men's table -->
             <div class="float-left">
-              <p class="text-2xl text-bg-blue-900">Men's</p>
-              <table class="w-full border-2 border-bg-blue-900">
-                <thead class="bg-blue-900">
-                  <tr>
-                    <th class="text-left p-3 text-white">Bunk<i class="bi-alarm"></i></th>
-                    <th class="text-left p-3 text-white">Guest</th>
-                  </tr>
-                </thead>
-                <tbody class="rounded">
-                  <!-- Loop for observation slots -->
-                  <tr v-for="bunk in filteredBunks('men')" :key="bunk.number">
-                    <td class="px-3">{{ bunk.number }}</td>
-                    <td class="px-3">
-                      <input type="text" v-model="bunk.name" @input="updateBunk(bunk.number, bunk.name)" class="border-2 border-gray-500 rounded px-3 py-1 w-full">
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+                <p class="text-2xl text-bg-blue-900">Lockers</p>
+                <table class="w-full border-2 border-bg-blue-900">
+                  <thead class="bg-blue-900">
+                    <tr>
+                      <th class="text-left p-3 text-white">Number<i class="bi-alarm"></i></th>
+                      <th class="text-left p-3 text-white">Guest</th>
+                    </tr>
+                  </thead>
+
+                </table>
             </div>
-            <div class="clearfix"></div> <!-- Clear the float -->
           </div>
         </Wrapper>
     </div>
@@ -120,7 +141,8 @@ import store from "../store/store.js"
       return {
         guests: [],
         reservations: [],
-        bunks: []
+        bunks: [],
+        viewBunks: true
       };
     },
     async created(){
@@ -208,6 +230,14 @@ import store from "../store/store.js"
             console.error("Error updating bunk:", error);
           });
         }
+      },
+      switchView(){
+        if(this.viewBunks){
+          this.viewBunks = false;
+        } else {
+          this.viewBunks = true;
+        }
+        
       }
     }   
   }
@@ -226,7 +256,7 @@ import store from "../store/store.js"
   }
 
   .checkout-btn {
-    position: absolute;
+    position:absolute;
     top: 8%;
     right: 1%;
     background-color: rgba(30,58,138,255);
@@ -239,9 +269,28 @@ import store from "../store/store.js"
     font-size: 16px;
     cursor: pointer;
     border-radius: 5px;
+    margin-top: 20px;
   }
 
-  .checkout-btn:hover {
+  .switch-btn {
+    position:absolute;
+    top: 8%;
+    left: 1%;
+    background-color: rgba(30,58,138,255);
+    border: none;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    cursor: pointer;
+    border-radius: 5px;
+    margin-top: 20px;
+  }
+
+  .checkout-btn:hover,
+  .switch-btn:hover {
     background-color: grey;
   }
 
